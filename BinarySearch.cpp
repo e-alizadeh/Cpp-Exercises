96a34d78e258628cb3e29db4e36bbd2923d77a75
Esmaeil Alizadeh <30320825+e-alizadeh@users.noreply.github.com>
2018-02-02 14:35:39 -0500

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,6 +1,10 @@
-﻿// BinarySearch.cpp : Defines the entry point for the console application.
-//
-
+﻿// BinarySearch.cpp 
+/* 
+This file performs the following operations using Binary Search:
+- Finding an element in an integer array,
+- Finding the first occurrence of an element in an integer array,
+- Finding the last occurrence of an element in an integer array.
+*/
 
 #include <iostream>
 using namespace std;
